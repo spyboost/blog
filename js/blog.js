f741f359d95c92fa208caab9d7a9e94d97700e88
spyboost <spyboost@gmail.com>
2013-10-27 17:14:37 -0400

    blog.css

---
+++ js/blog.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', function(){
+  if(navigator.platform.indexOf('Win') >= 0)
+    document.body.classList.add('os-windows');
+  $('.file .name').prepend('<i class="icon-collapse-alt"></i>');
+  var clickElements = document.querySelectorAll('.file i[class^=icon]');
+  function clickElementClickHandler(){
+    console.log(arguments, this);
+    var contentEl = $(this).parent().parent().find('.content');
+    if(this.dataset.collapsed){
+      delete this.dataset.collapsed;
+      this.className = 'icon-collapse-alt';
+      contentEl.fadeIn(200);
+    }else{
+      this.dataset.collapsed = true;
+      this.className = 'icon-expand-alt';
+      contentEl.fadeOut(200);
+    }
+  }
+  (function(){
+    for(var i = 0; i < clickElements.length; i++){
+      var clickElement = clickElements[i];
+      clickElement.addEventListener('click', clickElementClickHandler);
+    }
+  })();
+});
